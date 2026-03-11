65a5f8aff0d4f67fa7854858d6f6311f29b23354
AYush <103580263+This-iz-AYush@users.noreply.github.com>
2026-03-11 23:13:16 +0530

    Update dashboard.py

--- cogs/dashboard.py
+++ cogs/dashboard.py
@@ -23,6 +23,7 @@ class Dashboard(commands.Cog):
             web.get('/callback', self.callback),
             web.get('/logout', self.logout),
             web.get('/manage/{guild_id}', self.manage_server),
+            web.get('/logs/{guild_id}', self.server_logs), # <-- ADD THIS LINE
             web.get('/owner_panel', self.owner_panel),
             web.post('/api/settings/{guild_id}', self.update_settings),
             web.post('/api/ip_action', self.handle_ip_action),
