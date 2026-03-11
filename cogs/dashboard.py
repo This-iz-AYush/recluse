85efb752981f9687c9c4360b307173e845a605fd
AYush <103580263+This-iz-AYush@users.noreply.github.com>
2026-03-12 00:57:05 +0530

    Update dashboard.py

--- cogs/dashboard.py
+++ cogs/dashboard.py
@@ -31,7 +31,7 @@ class Dashboard(commands.Cog):
             web.get('/owner_panel', self.owner_panel),
             web.post('/api/settings/{guild_id}', self.update_settings),
             web.post('/api/ip_action', self.handle_ip_action),
-            web.post('/api/verify_visitor', self.verify_visitor)
+            web.post('/api/verify_visitor', self.verify_visitor),
             web.post('/api/owner_action', self.handle_owner_action)
         ])
         
